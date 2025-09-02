e82c5e04eb8f42d85582c5df6366b3fe96c101e2
Rickson Simoes <rickson.simoes@hotmail.com>
2025-09-02 14:48:06 -0300

    .Api initial controller layer/ AppSettings DB Config and Json policy

---
+++ src/RateMovie.Api/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace RateMovie.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MovieController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return Ok(new { Message = "Hello"});
+        }
+    }
+}
---
+++ src/RateMovie.Api/Program.cs
@@ -0,0 +1,42 @@
+
+namespace RateMovie.Api
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            //RateMovie Configs
+            builder.Services
+                .AddControllers()
+                .AddJsonOptions(opt => opt.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
