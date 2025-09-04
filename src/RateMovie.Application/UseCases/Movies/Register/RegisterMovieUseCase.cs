53550993712b8f26126bb8b8a71ec0344e4c5a2e
Rickson Simoes <rickson.simoes@hotmail.com>
2025-09-04 17:04:39 -0300

    Quick validation handler with generic argument exception (I'll implement an exception filter)

--- src/RateMovie.Application/UseCases/Movies/Register/RegisterMovieUseCase.cs
+++ src/RateMovie.Application/UseCases/Movies/Register/RegisterMovieUseCase.cs
@@ -8,6 +8,7 @@ namespace RateMovie.Application.UseCases.Movies.Register
     internal class RegisterMovieUseCase : IRegisterMovieUseCase
     {
         private readonly IMovieWriteOnlyRepository _movieRepository;
+
         public RegisterMovieUseCase(IMovieWriteOnlyRepository movieRepository)
         {
             _movieRepository = movieRepository;
@@ -15,6 +16,8 @@ namespace RateMovie.Application.UseCases.Movies.Register
 
         public async Task<ResponseMovieJson> Execute(RequestMovieJson req)
         {
+            ValidationHandler(req);
+
             ResponseMovieJson movieRequest = new()
             {
                 Name = req.Name,
@@ -32,6 +35,42 @@ namespace RateMovie.Application.UseCases.Movies.Register
             await _movieRepository.Register(movie);            
 
             return movieRequest;
+        }
+
+        private void ValidationHandler(RequestMovieJson req)
+        {
+            List<string> errors = [];
+
+            // Nullish
+            if (string.IsNullOrWhiteSpace(req.Name))
+            {
+                errors.Add("Name can't be null");
+            }
+
+            // Length
+            if (req.Name.Length > 90)
+            {
+                errors.Add("Movie name must not exceed 90 characters.");
+            }
+
+            if (req.Comment.Length > 700)
+            {
+                errors.Add("Movie comment must not exceed 700 characters.");
+            }
+
+            // Stars 1 to 5;
+            if (req.Stars < 1 || req.Stars > 5)
+            {
+                errors.Add("Stars must be between 1 and 5");
+            }
+
+            // Exception
+            if (errors.Count != 0)
+            {
+                string errs = string.Join(", ", errors);
+
+                throw new ArgumentException(errs);
+            }
         }
     }
 }
