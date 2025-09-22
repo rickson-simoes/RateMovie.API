d982dbe3873c0fdfacc88664691c37dd0746dd06
Rickson Simoes <rickson.simoes@hotmail.com>
2025-09-22 19:50:29 -0300

    Migrations cleanup - Seeds updated

--- src/RateMovie.Infraestructure/Migrations/20250922224918_InitialCreate.Designer.cs
+++ src/RateMovie.Infraestructure/Migrations/20250922224918_InitialCreate.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RateMovie.Infraestructure.DataAccess;
 
@@ -10,9 +11,11 @@ using RateMovie.Infraestructure.DataAccess;
 namespace RateMovie.Infraestructure.Migrations
 {
     [DbContext(typeof(RateMovieDBContext))]
-    partial class RateMovieDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250922224918_InitialCreate")]
+    partial class InitialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
