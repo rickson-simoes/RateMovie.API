6d20ba9308212b368ba6cc71679b3557da391c9d
Rickson Simoes <rickson.simoes@hotmail.com>
2026-01-14 12:21:34 -0300

    feat: new mapper -> User

---
+++ src/RateMovie.Application/Mapper/MovieMapperHelper.cs
@@ -0,0 +1,59 @@
+﻿using RateMovie.Communication.Requests;
+using RateMovie.Communication.Responses;
+using RateMovie.Domain.Entities;
+
+namespace RateMovie.Application.Mapper
+{
+    public static class MovieMapperHelper
+    {
+        public static ResponseShortMovieJson? ToResponseShortMovieJson(this Movie movie)
+        {
+            if (movie == null)
+                return null;
+
+            return new ResponseShortMovieJson
+            {
+                Id = movie.Id,
+                Name = movie.Name,
+                Stars = movie.Stars,
+            };
+        }
+
+        public static ResponseMovieJson ToResponseMovieJson(this Movie requestMovie)
+        {
+            return new ResponseMovieJson
+            {
+                Name = requestMovie.Name,
+                Comment = requestMovie.Comment,
+                Stars = requestMovie.Stars,
+            };
+        }
+
+        public static ResponseMovieJson ToResponseMovieJson(this RequestMovieJson requestMovie)
+        {
+            return new ResponseMovieJson
+            {
+               Name = requestMovie.Name,
+               Comment = requestMovie.Comment,
+               Stars = requestMovie.Stars,
+            };
+        }
+
+        public static Movie ToMovieEntity(this ResponseMovieJson responseMovie)
+        {
+            return new Movie
+            {
+                Name = responseMovie.Name,
+                Comment = responseMovie.Comment,
+                Stars = responseMovie.Stars
+            };
+        }
+
+        public static void GetRequestMovieData(this Movie movie, RequestMovieJson req)
+        {
+            movie.Name = req.Name;
+            movie.Comment = req.Comment;
+            movie.Stars = req.Stars;
+        }
+    }
+}
---
+++ src/RateMovie.Application/Mapper/UserMapperHelper.cs
@@ -0,0 +1,18 @@
+﻿using RateMovie.Communication.Requests;
+using RateMovie.Domain.Entities;
+
+namespace RateMovie.Application.Mapper
+{
+    public static class UserMapperHelper
+    {
+        public static User RequestAddUserJsonToUser(this RequestAddUserJson req)
+        {
+            return new User
+            {
+                Name = req.Name,
+                Email = req.Email,
+                Password = req.Password
+            };
+        }
+    }
+}
