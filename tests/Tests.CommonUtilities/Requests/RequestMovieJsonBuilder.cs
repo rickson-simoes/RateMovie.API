2338c9a2dbd8f9ba9b5f67b74371b2d38293f833
Rickson Simoes <rickson.simoes@hotmail.com>
2026-01-22 18:43:25 -0300

    tests: Movies validator - unit tests

---
+++ tests/Tests.CommonUtilities/Requests/RequestMovieJsonBuilder.cs
@@ -0,0 +1,18 @@
+﻿using Bogus;
+using RateMovie.Communication.Requests;
+using RateMovie.Communication.Enum;
+
+namespace Tests.CommonUtilities.Requests
+{
+    public class RequestMovieJsonBuilder
+    {
+        public static RequestMovieJson Build()
+        {
+            return new Faker<RequestMovieJson>()
+                .RuleFor(req => req.Name, f => f.Random.Word())
+                .RuleFor(req => req.Comment, f => f.Random.Words(count: 5))
+                .RuleFor(req => req.Stars, f => f.Random.Byte(1, 5))
+                .RuleFor(req => req.Genre, f => f.PickRandom<MovieGenre>());
+        }
+    }
+}
---
+++ tests/Tests.RateMovieApp/Movies/UnitTests/Validators/MoviesValidatorTest.cs
@@ -0,0 +1,128 @@
+﻿using FluentValidation.Results;
+using RateMovie.Application.UseCases.Movies;
+using RateMovie.Communication.Requests;
+using RateMovie.Exception;
+using Shouldly;
+using Tests.CommonUtilities.Requests;
+
+namespace Tests.RateMovieApp.Movies.UnitTests.Validators
+{
+    public class MoviesValidatorTest
+    {
+        [Fact]
+        public void Success()
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeTrue();
+            validator.Errors.ShouldBeEmpty();
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void Should_Fail_When_Movie_Name_Is_Null_Or_Empty(string name)
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            request.Name = null;
+
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeFalse();
+            validator
+                .Errors
+                .ShouldHaveSingleItem()
+                .ErrorMessage
+                .ShouldBe(ErrorMessagesResource.MOVIE_NAME_CANT_BE_NULL);
+        }
+
+        [Fact]
+        public void Should_Fail_When_Movie_Name_Is_Above_90_Characters()
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            request.Name = string.Create(91, 'A', (span, ch) => span.Fill(ch));
+
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeFalse();
+            validator
+                .Errors
+                .ShouldHaveSingleItem()
+                .ErrorMessage
+                .ShouldBe(ErrorMessagesResource.MOVIE_NAME_MAX_CHARACTER_LENGTH);
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        [InlineData("   ")]
+        public void Should_Succeed_When_Comment_Is_Null_Or_Empty(string comment)
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            request.Comment = comment;
+
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeTrue();
+            validator.Errors.ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void Should_Fail_If_Comment_Exceeds_700_Characters()
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            request.Comment = string.Create(701, 'A', (span, ch) => span.Fill(ch));
+
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeFalse();
+            validator
+                .Errors
+                .ShouldHaveSingleItem()
+                .ErrorMessage
+                .ShouldBe(ErrorMessagesResource.MOVIE_COMMENT_MAX_CHARACTER_LENGTH);
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(6)]
+        [InlineData(9)]
+        public void Should_Fail_When_Stars_Are_Out_Of_Range(byte stars)
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            request.Stars = stars;
+
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeFalse();
+            validator
+                .Errors
+                .ShouldHaveSingleItem()
+                .ErrorMessage
+                .ShouldBe(ErrorMessagesResource.MOVIE_STARS_BETWEEN_RANGE);
+        }
+
+        [Fact]
+        public void Should_Fail_When_Genre_Is_Invalid()
+        {
+            var request = RequestMovieJsonBuilder.Build();
+            request.Genre = (RateMovie.Communication.Enum.MovieGenre)8;
+
+            var validator = Validation(request);
+
+            validator.IsValid.ShouldBeFalse();
+            validator
+                .Errors
+                .ShouldHaveSingleItem()
+                .ErrorMessage
+                .ShouldBe(ErrorMessagesResource.MOVIE_GENRE_INVALID);
+        }
+
+        private ValidationResult Validation(RequestMovieJson request)
+        {
+            return new MoviesValidator().Validate(request);
+        }
+    }
+}
